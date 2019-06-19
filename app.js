55eb08ac9849335a62848beef973c289faad9d65
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-19 02:03:05 -0600

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 // Make an array right here
-var topics = ["Beyonce","Billie Eilish","Kehlani","Drake","Ciara"];
+// var topics = ["Beyonce","Billie Eilish","Kehlani","Drake","Ciara"]
+
+// function array() {
+//   for (var i = 0; i < topics.length; i++) {
+//     document.getElementById("#gifs-appear-here").innerHTML += "<button>" + topics[i] + "</button>";
+//   }
+// }
+//   array();
 
 
-$("button").on("click",function(){
+
+
+$("#button").on("click",function(){
     var topics = $(this).attr(data-topics);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + 
-    "&api_key=ql8qKxXmsskjoZ5qPEe9lGiyQyvlvONu";
+    "&api_key=ql8qKxXmsskjoZ5qPEe9lGiyQyvlvONu&limit=10";
 
     $.ajax({
+        url: queryURL,
+        method: "GET"
 
-    })
+    }).then(function(response){
+     var results = response.data;
+    
+     for (var i = 0; i < results.length; i++){
+         var topicsDiv = $("<div>");
+        
+         var p = $("<p>").text("Rating: " + results[i].rating);
+         
+         var topicImage = $("<img>");
+         
+         topicImage.attr("src", results[i].images.fixed_height.url);
+         
+         topicsDiv.append(p);
+          
+         topicsDiv.append(topicsImage);
+          
+          $("#gifs-appear-here").prepend(topicsDiv);
+     }
+    
+    });
+    
 });
 
 
